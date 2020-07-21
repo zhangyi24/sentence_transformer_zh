65ac355b2eb7b3189a77ceeb611813c5575e90f1
张毅 <zhangyi24stu@qq.com>
2020-07-21 19:08:27 +0800

    evaluate on sgcc_zj intent recognition dataset

--- examples/applications/intent_recognition.py
+++ examples/applications/intent_recognition.py
@@ -7,24 +7,21 @@ we want to find the most similar sentence in this corpus.
 This script outputs for various queries the top 5 most similar sentences in the corpus.
 """
 import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+
 import csv
 import logging
 import pickle
 import time
+import argparse
 
 import scipy.spatial
 import numpy as np
 
 from sentence_transformers import SentenceTransformer
 
-print('loading model...')
-model = SentenceTransformer('../training_transformers/output/training_cmnli_bert-base-chinese-2020-07-20_17-35-51')
-
-datasets_dir = '../datasets/intent/zj'
-trainset_path = os.path.join(datasets_dir, 'train.tsv')
-devset_path = os.path.join(datasets_dir, 'dev.tsv')
-testset_path = os.path.join(datasets_dir, 'test.tsv')
-embedding_cache_path = 'embeddings.pkl'
 
 def get_dataset(dataset_path):
     sents = []
@@ -39,78 +36,44 @@ def get_dataset(dataset_path):
     return sents, labels
 
 
-train_sents, train_labels = get_dataset(trainset_path)
-dev_sents, dev_labels = get_dataset(devset_path)
-test_sents, test_labels = get_dataset(testset_path)
+def evaluate(corpus, corpus_embeddings, queries, labels):
+    print('encoding queries...')
+    query_embeddings = model.encode(queries)
+    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    print('KNN...')
+    distances_matrix = scipy.spatial.distance.cdist(query_embeddings, corpus_embeddings, "cosine")
+    pred_idx = np.argmin(distances_matrix, axis=1)
+    true_num = 0
+    for query_id, query in enumerate(queries):
+        pred_id = pred_idx[query_id]
+        if labels[query_id] == train_labels[pred_id]:
+            true_num += 1
+    return true_num / len(queries)
 
-corpus = train_sents
-if not os.path.exists(embedding_cache_path):
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", "-m", default="bert-base-chinese", type=str)
+    args = parser.parse_args()
+    print('loading model: %s...' % args.model_name)
+    model = SentenceTransformer(args.model_name)
+
+    datasets_dir = '../datasets/intent/zj'
+    trainset_path = os.path.join(datasets_dir, 'train.tsv')
+    devset_path = os.path.join(datasets_dir, 'dev.tsv')
+    testset_path = os.path.join(datasets_dir, 'test.tsv')
+    train_sents, train_labels = get_dataset(trainset_path)
+    dev_sents, dev_labels = get_dataset(devset_path)
+    test_sents, test_labels = get_dataset(testset_path)
+
+    corpus = train_sents
     print('encoding corpus...')
     corpus_embeddings = model.encode(corpus)
-    print('dumping corpus embeddings...')
-    with open(embedding_cache_path, 'wb') as f:
-        pickle.dump(corpus_embeddings, f)
-else:
-    print('loading corpus embeddings...')
-    with open(embedding_cache_path, 'rb') as f:
-        corpus_embeddings = pickle.load(f)
 
+    print('\nevalueting on devset:')
+    acc = evaluate(corpus, corpus_embeddings, dev_sents, dev_labels)
+    print('acc: {:.4%}'.format(acc))
 
-
-# # Query sentences:
-# print('encoding queries...')
-# queries = ['没钱没钱']
-# query_embeddings = model.encode(queries)
-#
-# # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-# print('KNN...')
-# for query, query_embedding in zip(queries, query_embeddings):
-#     distances = scipy.spatial.distance.cdist([query_embedding], train_sents_embeddings, "cosine")[0]
-#
-#     results = zip(range(len(distances)), distances)
-#     results = sorted(results, key=lambda x: x[1])
-#
-#     print("\n\n======================\n\n")
-#     print("Query:", query)
-#     print("\nTop 5 most similar sentences in corpus:")
-#
-#     for idx, distance in results[0:5]:
-#         print(corpus[idx].strip(), "(Score: %.4f)" % (1-distance))
-
-# Query sentences:
-print('encoding queries...')
-queries = test_sents
-query_embeddings = model.encode(queries)
-# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-print('KNN...')
-distances_matrix = scipy.spatial.distance.cdist(query_embeddings, corpus_embeddings, "cosine")
-pred_idx = np.argmin(distances_matrix, axis=1)
-true_num = 0
-for query_id, query in enumerate(queries):
-    pred_id = pred_idx[query_id]
-    nearest_sent = corpus[pred_id]
-    if test_labels[query_id] == train_labels[pred_id]:
-        true_num += 1
-    # print(query, nearest_sent, test_labels[query_id], train_labels[pred_id])
-print(true_num / len(queries))
-
-
-# Query sentences:
-print('encoding queries...')
-queries = dev_sents
-query_embeddings = model.encode(queries)
-# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-print('KNN...')
-distances_matrix = scipy.spatial.distance.cdist(query_embeddings, corpus_embeddings, "cosine")
-pred_idx = np.argmin(distances_matrix, axis=1)
-true_num = 0
-for query_id, query in enumerate(queries):
-    pred_id = pred_idx[query_id]
-    nearest_sent = corpus[pred_id]
-    if dev_labels[query_id] == train_labels[pred_id]:
-        true_num += 1
-    # print(query, nearest_sent, dev_labels[query_id], train_labels[pred_id])
-print(true_num / len(queries))
-
-
-
+    print('\nevalueting on testset:')
+    acc = evaluate(corpus, corpus_embeddings, test_sents, test_labels)
+    print('acc: {:.4%}'.format(acc))
--- examples/training_transformers/training_csts_continue_training.py
+++ examples/training_transformers/training_csts_continue_training.py
@@ -7,12 +7,13 @@ If you want to fine-tune a huggingface/transformers model like bert-base-uncased
 """
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))
 
 import argparse
 from torch.utils.data import DataLoader
 import math
-from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses
+from sentence_transformers import SentenceTransformer, SentencesDataset, LoggingHandler, losses
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 from sentence_transformers.readers import CSTSBenchmarkDataReader
 import logging
@@ -20,7 +21,8 @@ from datetime import datetime
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", "-m", default='output/cmnli_bert-base-chinese', type=str, help="model name or model dir")
+    parser.add_argument("--model", "-m", default='output/bert-base-chinese-cmnli-mean', type=str,
+                        help="model name or model dir")
     parser.add_argument("--num_epochs", "-e", default=10, type=int, help="number of epochs")
     args = parser.parse_args()
     model_name = args.model
@@ -32,9 +34,8 @@ if __name__ == '__main__':
                         handlers=[LoggingHandler()])
     #### /print debug information to stdout
 
-
     train_batch_size = 16
-    model_save_path = 'output/training_csts_continue_training-'+model_name+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_save_path = 'output/training-csts-' + model_name.rstrip("/").split('/')[-1] + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     csts_reader = CSTSBenchmarkDataReader('../datasets/CSTS-B', normalize_scores=True)
 
     # Load a pre-trained sentence transformer model
@@ -46,17 +47,14 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(train_data, shuffle=True, batch_size=train_batch_size)
     train_loss = losses.CosineSimilarityLoss(model=model)
 
-
     logging.info("Read CSTS-B dev dataset")
     dev_data = SentencesDataset(examples=csts_reader.get_examples('cnsd-sts-dev.txt'), model=model)
     dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=train_batch_size)
     evaluator = EmbeddingSimilarityEvaluator(dev_dataloader)
 
-
     # Configure the training. We skip evaluation in this example
-    warmup_steps = math.ceil(len(train_data)*num_epochs/train_batch_size*0.1) #10% of train data for warm-up
+    warmup_steps = math.ceil(len(train_data) * num_epochs / train_batch_size * 0.1)  # 10% of train data for warm-up
     logging.info("Warmup-steps: {}".format(warmup_steps))
-
 
     # Train the model
     model.fit(train_objectives=[(train_dataloader, train_loss)],
@@ -65,13 +63,6 @@ if __name__ == '__main__':
               evaluation_steps=100,
               warmup_steps=warmup_steps,
               output_path=model_save_path)
-
-
-    ##############################################################################
-    #
-    # Load the stored model and evaluate its performance on CSTS-B dataset
-    #
-    ##############################################################################
 
     model = SentenceTransformer(model_save_path)
     test_data = SentencesDataset(examples=csts_reader.get_examples("cnsd-sts-test.txt"), model=model)
